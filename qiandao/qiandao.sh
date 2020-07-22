eaa2ea10b5b5f6acf460c2f31afe8e0d6832b077
sunliang <sunliang@rj.com>
2020-07-22 15:38:29 +0800

    u

--- qiandao/qiandao.sh
+++ qiandao/qiandao.sh
@@ -1,4 +1,4 @@
 LOGFILE=/home/node/logs/qiandao.log
 PIDFILE=/home/node/pid/qiandao.pid
 
-nohup npm start > $LOGFILE & echo $! > $PIDFILE
+nohup node /home/node/sunNodeLearning/qiandao/index.js > $LOGFILE & echo $! > $PIDFILE
