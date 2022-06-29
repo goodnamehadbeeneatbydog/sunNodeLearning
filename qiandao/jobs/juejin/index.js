dbebc0ee0278acb7f39f4c2e8252e24ee8876799
liang.sun <44958918@qq.com>
2022-06-29 08:44:59 +0800

    ++++++

--- qiandao/jobs/juejin/index.js
+++ qiandao/jobs/juejin/index.js
@@ -2,7 +2,7 @@
  * @Author: liang.sun 44958918@qq.com
  * @Date: 2022-05-26 09:24:51
  * @LastEditors: liang.sun 44958918@qq.com
- * @LastEditTime: 2022-06-28 15:40:14
+ * @LastEditTime: 2022-06-29 08:43:25
  * @FilePath: \sunNodeLearning\qiandao\juejin\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,7 @@ const url =
 const moment = require("moment");
 var CronJob = require("cron").CronJob;
 
-const juejinJob = new CronJob("0 5 7 * * *", async function () {
+const juejinJob = new CronJob("0 3 8 * * *", async function () {
     console.log("开始掘金签到  ：" + moment().format("YYYY-MM-DD hh:mm:ss"));
     const result = await juejin();
 
