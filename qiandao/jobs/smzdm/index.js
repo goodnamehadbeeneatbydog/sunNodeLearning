a32bf7502ad50b3be6bb9c3ba1171ba0344d674c
liang.sun <44958918@qq.com>
2023-01-31 08:45:40 +0800

    fix++

--- qiandao/jobs/smzdm/index.js
+++ qiandao/jobs/smzdm/index.js
@@ -2,7 +2,7 @@
  * @Author: liang.sun 44958918@qq.com
  * @Date: 2023-01-10 12:40:23
  * @LastEditors: liang.sun 44958918@qq.com
- * @LastEditTime: 2023-01-31 08:39:29
+ * @LastEditTime: 2023-01-31 08:45:32
  * @FilePath: \sunNodeLearning\qiandao\jobs\smzdm\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -67,8 +67,8 @@ async function qiandao() {
 
 
 //测试使用，上线注释
- console.log('服务器启动中=====  ：'+moment().format("YYYY-MM-DD hh:mm:ss"));
- job.start();
- console.log('服务器启动完成=====  ：'+moment().format("YYYY-MM-DD hh:mm:ss"));
+//  console.log('服务器启动中=====  ：'+moment().format("YYYY-MM-DD hh:mm:ss"));
+//  job.start();
+//  console.log('服务器启动完成=====  ：'+moment().format("YYYY-MM-DD hh:mm:ss"));
 
 module.exports = job
